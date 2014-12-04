26f8abdee8e3943571a2c8df6e07a7d80a855e0c
Date Of Khaleesi <thismuller@gmail.com>
2014-12-04 11:36:00 +0300

    Update Access_modifiers.java

--- Access_Modifiers.java/Access_modifiers.java
+++ Access_Modifiers.java/Access_modifiers.java
@@ -5,4 +5,10 @@ There are two levels of access control:
     At the top level—public, or package-private (no explicit modifier).
     At the member level—public, private, protected, or package-private (no explicit modifier).
 
+Modifier 	Class 	Package 	Subclass 	World
+public 	    Y       	Y 	        Y 	     Y
+protected 	Y       	Y 	        Y 	     N
+no modifier Y 	        Y 	        N 	     N
+private 	Y 	        N 	        N 	     N
+
 */
