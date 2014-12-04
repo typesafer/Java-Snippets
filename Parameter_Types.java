e5d49b008fcdf434e6daa8a636e7c883cd1fc867
Date Of Khaleesi <thismuller@gmail.com>
2014-12-04 11:19:04 +0300

    Create Parameter_Types.java

---
+++ Parameter_Types.java
@@ -0,0 +1,8 @@
+/*
+ You can use any data type for a parameter of a method or a constructor. This includes primitive data types, such as doubles, floats, and integers, as you saw in the 
+ computePayment method, and reference data types, such as objects and arrays. 
+*/
+
+public Polygon polygonFrom(Point[] corners) {
+    // method body goes here
+}
