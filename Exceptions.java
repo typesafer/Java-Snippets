a09f58410f86e1db6520e4a6ffeae66436c03d3e
Date Of Khaleesi <thismuller@gmail.com>
2014-12-04 13:10:26 +0300

    Create Exceptions.java

---
+++ Exceptions.java
@@ -0,0 +1,4 @@
+The term exception is shorthand for the phrase "exceptional event."
+Definition:  An exception is an event, which occurs during the execution of a program, that disrupts the normal flow of the program's instructions.
+
+When an error occurs within a method, the method creates an object and hands it off to the runtime system. The object, called an exception object, contains information about the error, including its type and the state of the program when the error occurred. Creating an exception object and handing it to the runtime system is called throwing an exception. 
