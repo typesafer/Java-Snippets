f315ffe35150438a686869c8e3b597f4d65d2a7a
Date Of Khaleesi <thismuller@gmail.com>
2014-12-04 10:43:08 +0300

    Create Notes.java

---
+++ Notes.java
@@ -0,0 +1,7 @@
+/**
+1. Member variables in a class—these are called fields.
+
+2. Variables in a method or block of code—these are called local variables.
+
+3. Variables in method declarations—these are called parameters. 
+*/
